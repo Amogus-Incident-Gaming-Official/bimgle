eff3c861c030ed25d5d205d5cba9bdebc4264f80
duckycrater <76639165+duckycrater@users.noreply.github.com>
2025-03-12 12:46:31 -0400

    when the mushroom is stew

---
+++ kubejs/server_scripts/mushroomstewrecipe.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(mushroomstew => {
+  desertmedal.shapeless(
+  Item.of('minecraft.mushroom_stew', 1), // arg 1: output
+  [
+    'minecraft:red_mushroom',
+    'minecraft:brown_mushroom',  //arg 3: the mapping object
+    'minecraft:bowl'
+  ]
+)
+})
